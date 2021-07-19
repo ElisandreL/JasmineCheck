b66aa4c9b4674875c95b7f1be72c5e1f35f30dd3
Elisandre <elisandre.laenens@gmail.com>
2021-07-19 15:13:54 +0200

    premier fini

--- 3.Jasmine/3.1.Test1/questions.js
+++ 3.Jasmine/3.1.Test1/questions.js
@@ -8,55 +8,60 @@ let remplaceECar = (texte) => {
     return text;
 }
 let concatString = (texte1, texte2) => {
-
+    return texte1 + texte2;
 }
 let afficherCar5 =  (texte) => {
-
+    return texte.charAt(4);
 }
 let afficher9Car =  (texte) => {
-
+    return texte.slice(0, 9);
 }
 let majusculeString =  (texte) => {
-
+    return texte.toUpperCase();
 }
 let minusculeString =  (texte) => {
-
+    return texte.toLowerCase();
 }
 let SupprEspaceString =  (texte) => {
+    return texte.slice(1, (texte.length -1));
 
 }
 let IsString =  (texte) => {
-
-}
+    if(typeof texte === 'string') {
+       return true
+    } 
+} 
 
 let AfficherExtensionString =  (texte) => {
-
+    return texte.split('.').pop();
 }
 let NombreEspaceString =  (texte) => {
-
+    return texte.split(" ").length - 1;
 }
 let InverseString =  (texte) => {
-
+    return texte.split('').reverse().join('');
 }
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
 let calculPuissance =  (x, y) => {
-
+    return Math.pow(x, y);
 }
 let valeurAbsolue =  (nombre) => {
-
+    return Math.abs(nombre);
 }
 let valeurAbsolueArray =  (array) => {
-
+    return array.map(Math.abs);
 }
 let sufaceCercle =  (rayon) => {
-
+    let surf = Math.PI*rayon*rayon;
+    return Math.round(surf);
 }
 let hypothenuse =  (ab, ac) => {
-
+    return Math.hypot(ab, ac);
 }
 let calculIMC =  (poids, taille) => {
-
+    let imc = poids / (taille * taille);
+    return Math.round(imc * 100) / 100;
 }
